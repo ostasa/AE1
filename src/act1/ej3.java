b202fbf7835f8132154209c4bfae8d67047faaf2
ALUMNADO\ostasa <ostasa@floridauniversitaria.es>
2021-09-22 19:53:57 +0200

    Terminado AE1-3

--- src/act1/ej3.java
+++ src/act1/ej3.java
@@ -1,10 +1,19 @@
 package act1;
 
+import java.util.Scanner;
+
 public class ej3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner x = new Scanner(System.in);
+		System.out.println("Introduce el numero");
+		int numero= x.nextInt();
+		int num=0;
+		for(int i = 0;i<numero;i++) {
+			num+=2;
+		}
+		System.out.println(num);
 	}
 
 }
