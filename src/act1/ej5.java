0e0c88465459da4c49f5c78dae4c5a130548c32a
ALUMNADO\ostasa <ostasa@floridauniversitaria.es>
2021-09-22 20:17:00 +0200

    Creando AE1-5

---
+++ src/act1/ej5.java
@@ -0,0 +1,17 @@
+package act1;
+
+import java.util.Scanner;
+
+public class ej5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner x = new Scanner(System.in);
+		int gg = x.nextInt();
+ double[] num = new double[gg];
+ for(int i =0;i<gg;i++) {
+	 num[i]=Math.random(1)*50;
+ }
+	}
+
+}
