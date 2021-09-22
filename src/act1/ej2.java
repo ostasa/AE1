6981a09f44c33eb796f0deaa6614d5d6c9791a53
ALUMNADO\ostasa <ostasa@floridauniversitaria.es>
2021-09-22 19:29:36 +0200

    Terminado AE-2

--- src/act1/ej2.java
+++ src/act1/ej2.java
@@ -1,10 +1,24 @@
 package act1;
 
+import java.util.ArrayList;
+
 public class ej2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] array = new String[] {"Fran","Enrique","Victor", "Alex","Aleixandre","Ximo"};
+		for(int i =0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		
+		ArrayList<E> comp = new ArrayList();
+		comp.add("Fran");
+		comp.add("Enrique");
+		comp.add("Victor");
+		comp.add("Alex");
+		comp.add("Aleixandre");
+		comp.add("Ximo");
+		
 	}
 
 }
