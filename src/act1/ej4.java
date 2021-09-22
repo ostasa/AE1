9b2eccea91cd33bfb0636913a213719825f73dcc
ALUMNADO\ostasa <ostasa@floridauniversitaria.es>
2021-09-22 20:08:18 +0200

    Terminando AE1-4

--- src/act1/ej4.java
+++ src/act1/ej4.java
@@ -4,7 +4,11 @@ public class ej4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ long fact=1;
+ for(int i=1;i<=15;i++) {
+	 fact*=i;
+ }
+ System.out.println("El factorial es: "+fact);
 	}
 
 }
